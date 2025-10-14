81a5e339b9539556adb8975cfadec76e66a8d75e
Hajime4life <doguziagerman@gmail.com>
2025-10-14 11:02:41 +0300

    [feature] added exit confirm alert

--- iOS-FakeNFT-Extended/UI/Profile/Extentions/View+Ext.swift
+++ iOS-FakeNFT-Extended/UI/Profile/Extentions/View+Ext.swift
@@ -6,10 +6,16 @@
 //
 
 import SwiftUICore
+import UIKit
 
-/// Расширение для приминения стилей на вью
+
 extension View {
+    /// Расширение для приминения стилей на вью
     func applyTextInputStyle() -> some View {
         modifier(TextInputStyle())
+    }
+    /// Для управления навигацией, чтобы прервать переход назад если данные изменены на экране
+    func introspectNavigationController(_ callback: @escaping (UINavigationController) -> Void) -> some View {
+        self.background(NavigationControllerIntrospection(callback: callback))
     }
 }
