8c040e149459f222a6fab7fda2af026e08fda353
Симонов Иван дмитриевич <fensiz@me.com>
2025-10-18 16:49:05 +0300

    [fix] removed spaces

--- iOS-FakeNFT-Extended/iOSFakeNFTExtendedApp.swift
+++ iOS-FakeNFT-Extended/iOSFakeNFTExtendedApp.swift
@@ -2,25 +2,25 @@ import SwiftUI
 
 @main
 struct FakeNFTExtendedApp: App {
-    init() {
-        let tabAppearance = UITabBarAppearance()
-        tabAppearance.configureWithDefaultBackground()
-        tabAppearance.backgroundColor = .ypWhite
-        tabAppearance.shadowColor = .clear
-        UITabBar.appearance().standardAppearance = tabAppearance
-        UITabBar.appearance().scrollEdgeAppearance = tabAppearance
+	init() {
+		let tabAppearance = UITabBarAppearance()
+		tabAppearance.configureWithDefaultBackground()
+		tabAppearance.backgroundColor = .ypWhite
+		tabAppearance.shadowColor = .clear
+		UITabBar.appearance().standardAppearance = tabAppearance
+		UITabBar.appearance().scrollEdgeAppearance = tabAppearance
 
-        let navAppearance = UINavigationBarAppearance()
+		let navAppearance = UINavigationBarAppearance()
 		navAppearance.configureWithTransparentBackground()
-        navAppearance.backgroundColor = .ypWhite
-        UINavigationBar.appearance().standardAppearance = navAppearance
-        UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
-    }
+		navAppearance.backgroundColor = .ypWhite
+		UINavigationBar.appearance().standardAppearance = navAppearance
+		UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
+	}
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(ServicesAssembly(networkClient: DefaultNetworkClient(), nftStorage: NftStorageImpl()))
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+				.environment(ServicesAssembly(networkClient: DefaultNetworkClient(), nftStorage: NftStorageImpl()))
+		}
+	}
 }
