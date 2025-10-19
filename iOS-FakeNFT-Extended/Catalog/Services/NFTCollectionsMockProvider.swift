ccbfada2d9e3bc2d4d5d759992986ead7eac0d83
Vladimir <bizin.vladimir01@gmail.com>
2025-10-19 20:39:17 +0300

    [refactor]: udpate service for new models

--- iOS-FakeNFT-Extended/Catalog/Services/NFTCollectionsMockProvider.swift
+++ iOS-FakeNFT-Extended/Catalog/Services/NFTCollectionsMockProvider.swift
@@ -14,48 +14,54 @@ protocol NFTCollectionsProviderProtocol: Sendable {
 
 actor NFTCollectionsMockProvider: NFTCollectionsProviderProtocol {
 
-    private let nftCollections: [NFTCollectionModel] = [
-        .init(
-            imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/Brown.png"),
-            title: "singulis epicuri",
-            nftIDs: [1, 2, 3 ],
-            description: "curabitur ...",
-            authorID: 1567,
-            id: 123
-        ),
-        .init(
-            imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/Beige.png"),
-            title: "Beige",
-            nftIDs: [1, 2, 3, 4],
-            description: "A series of ...",
-            authorID: 49,
-            id: 504
-        ),
-        .init(
-            imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/White.png"),
-            title: "unum reque",
-            nftIDs: [1, 2],
-            description: "dictas ",
-            authorID: 76,
-            id: 683
-        )
-    ]
+	private static let mockNFT = NFTModel(
+		id: UUID(),
+		name: "Myrna Cervantes",
+		imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Ellsa/1.png")!,
+		rating: 5,
+		price: 39.37,
+		currency: .eth
+	)
 
-	private func getNFT(id: NFTCollectionModel.ID) -> NFTCollectionModel {
-        let nftCollection = nftCollections.randomElement()!
-        return .init(
-            imageURL: nftCollection.imageURL,
-            title: nftCollection.title,
-            nftIDs: nftCollection.nftIDs,
-            description: nftCollection.description,
-            authorID: nftCollection.authorID,
-            id: id
-        )
-    }
+	private static let mockAuthor = NFTUserModel(
+		id: UUID(),
+		name: "Jimmie Reilly",
+		description: "daddsd",
+		nfts: [],
+		websiteURL: URL(string: "https://student7.students.practicum.org")!,
+		avatarURL: URL(string: "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/594.jpg")!
+	)
+
+    private let nftCollections: [NFTCollectionModel] = [
+		.init(
+			id: UUID(),
+			title: "singulis epicuri",
+			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/Brown.png")!,
+			nfts: Array(repeating: mockNFT, count: 3),
+			description: "curabitur ...",
+			author: mockAuthor
+		),
+		.init(
+			id: UUID(),
+			title: "Beige",
+			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/Beige.png")!,
+			nfts: Array(repeating: mockNFT, count: 4),
+			description: "A series of ...",
+			author: mockAuthor
+		),
+		.init(
+			id: UUID(),
+			title: "unum reque",
+			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Обложки_коллекций/White.png")!,
+			nfts: Array(repeating: mockNFT, count: 2),
+			description: "dictas ...",
+			author: mockAuthor
+		)
+    ]
 
 	func fetch(number: Int) async throws -> [NFTCollectionModel] {
         try? await Task.sleep(for: .seconds(3))
-        return (0..<number).map { _ in Int.random(in: 0..<10_000_000) }.map { getNFT(id: $0)}
+		return (0..<number).map { _ in nftCollections.randomElement()! }
     }
 
 }
