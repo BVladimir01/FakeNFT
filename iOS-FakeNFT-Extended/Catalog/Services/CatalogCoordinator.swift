1b6d4e546eccab8f21cba02d8abf12bf4b17584e
Vladimir <bizin.vladimir01@gmail.com>
2025-10-20 15:11:18 +0300

    [feature]: add coordinator

---
+++ iOS-FakeNFT-Extended/Catalog/Services/CatalogCoordinator.swift
@@ -0,0 +1,46 @@
+//
+//  CatalogCoordinator.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Vladimir on 20.10.2025.
+//  Copyright © 2025 com.example. All rights reserved.
+//
+
+import Foundation
+
+@MainActor
+protocol CatalogCoordinatorProtocol {
+	func goBack()
+	func showDetails(for collection: NFTCollectionModel, coordinator: any CatalogCoordinatorProtocol)
+	func showWebView(for url: URL)
+}
+
+final class CatalogCoordinator: CatalogCoordinatorProtocol {
+
+	private let rootCoordinator: any RootCoordinator
+
+	init(rootCoordinator: any RootCoordinator) {
+		self.rootCoordinator = rootCoordinator
+	}
+
+	func goBack() {
+		rootCoordinator.goBack()
+	}
+
+	func showDetails(
+		for collection: NFTCollectionModel,
+		coordinator: any CatalogCoordinatorProtocol
+	) {
+		rootCoordinator.open(
+			screen: .collectionDetails(
+				collection: collection,
+				coordinator: coordinator
+			)
+		)
+	}
+
+	func showWebView(for url: URL) {
+		rootCoordinator.open(screen: .web(url: url))
+	}
+
+}
