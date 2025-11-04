cfae7925b744eb34ec368b0942b6e64cc441c134
Vladimir <bizin.vladimir01@gmail.com>
2025-11-04 12:01:35 +0300

    [refactor]: move netwroking service files into dedicated group

--- iOS-FakeNFT-Extended/App/iOSFakeNFTExtendedApp.swift
+++ iOS-FakeNFT-Extended/App/iOSFakeNFTExtendedApp.swift
@@ -24,7 +24,6 @@ struct FakeNFTExtendedApp: App {
 				EmptyView()
 			} else {
 				ContentView()
-					.environment(ServicesAssembly(networkClient: DefaultNetworkClient(), nftStorage: NftStorageImpl()))
 			}
 		}
 	}
